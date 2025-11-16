b29b43ba763074770b7efe499cdf847f6e89406f
Eric Halvorsen <Viscousness@Gmail.com>
2025-11-15 20:02:47 -0500

    Nvim Config

---
+++ lua/eric/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    popup_border_style = 'rounded',
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
+      filtered_items = {
+        -- visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignore = false,
+      },
+      window = {
+        position = 'left',
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
